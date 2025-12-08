962c67c72095e036e612253ff2c1c8a2fadff193
Andy Cloke <apcloke@gmail.com>
2025-12-08 13:22:20 +0000

    Documentation edits made through Mintlify web editor

---
+++ plausible.js
@@ -0,0 +1,15 @@
+// Privacy-friendly analytics by Plausible
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://plausible.io/js/pa-BnPL_Sb4qL_kTEiq5gLGX.js';
+  document.head.appendChild(script);
+  
+  window.plausible = window.plausible || function() {
+    (plausible.q = plausible.q || []).push(arguments);
+  };
+  plausible.init = plausible.init || function(i) {
+    plausible.o = i || {};
+  };
+  plausible.init();
+})();
